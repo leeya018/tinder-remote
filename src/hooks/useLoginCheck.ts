bc41e43c04236a9e3715571811e4d25a2cb66c34
leeya018 <leeyahav018@gmail.com>
2024-02-01 19:38:17 +0200

    build

--- src/hooks/useLoginCheck.ts
+++ src/hooks/useLoginCheck.ts
@@ -1,15 +1,15 @@
-import { UserAuth } from "@/context/AuthContext"
+// import { UserAuth } from "@/context/AuthContext"
 import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 
 const useLoginCheck = () => {
-  const { user } = UserAuth()
-  const router = useRouter()
+  // const { user } = UserAuth()
+  // const router = useRouter()
 
   useEffect(() => {
-    if (!user) {
-      router.push("/login")
-    }
+    // if (!user) {
+    //   router.push("/login")
+    // }
   }, [])
 }
 
