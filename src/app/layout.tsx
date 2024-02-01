6ec8b267ea80e7010d460d65cd063976c5218c66
leeya018 <leeyahav018@gmail.com>
2024-02-01 19:32:37 +0200

    change some config to make it work (firebase and auth)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { AuthContextProvider } from "@/context/AuthContext"
+// import { AuthContextProvider } from "@/context/AuthContext"
 import React from "react"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -20,7 +20,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {" "}
-        <AuthContextProvider>{children} </AuthContextProvider>
+        <div>{children} </div>
+        {/* <AuthContextProvider>{children} </AuthContextProvider> */}
       </body>
     </html>
   )
